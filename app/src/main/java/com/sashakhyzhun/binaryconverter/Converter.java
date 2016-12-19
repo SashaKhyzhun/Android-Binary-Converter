514032d8ba4fd491b84c01002887388cdd691e76
SashaKhyzhun <sasha.khyzhun@gmail.com>
2016-12-19 22:55:50 +0200

    refactored

--- app/src/main/java/com/sashakhyzhun/binaryconverter/Converter.java
+++ app/src/main/java/com/sashakhyzhun/binaryconverter/Converter.java
@@ -12,7 +12,7 @@ public class Converter extends Activity {
      * DECIMAL TO BINARY
      */
 
-    public String converterDecimalParaBinary(long value) {
+    public String converterDecimalToBinary(long value) {
         long offset = -1;
         StringBuilder sb = new StringBuilder();
 
@@ -33,7 +33,7 @@ public class Converter extends Activity {
      * DECIMAL TO OCTAL
      */
 
-    public String converterDecimalParaOctal(long value) {
+    public String converterDecimalToOctal(long value) {
         long offset = -1;
         StringBuilder sb = new StringBuilder();
 
@@ -54,18 +54,18 @@ public class Converter extends Activity {
      * DECIMAL TO HEXADECIMAL
      */
 
-    public String converterDecimalParaHexadecimal(long valor) {
+    public String converterDecimalToHexadecimal(long value) {
         char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
         long offset = -1;
         StringBuilder sb = new StringBuilder();
 
-        if (valor == 0) {
+        if (value == 0) {
             return "0";
         }
 
-        while (valor > 0) {
-            offset = (long) (valor % 16);
-            valor = valor / 16;
+        while (value > 0) {
+            offset = (long) (value % 16);
+            value = value / 16;
             sb.insert(0, hex[(int) offset]);
         }
 
@@ -76,21 +76,21 @@ public class Converter extends Activity {
      * BINARY TO DECIMAL
      */
 
-    public long converterBinaryParaDecimal(String valorBinary) {
-        long valor = 0;
+    public long converterBinaryToDecimal(String valueBinary) {
+        long offset = 0;
 
-        for (int i = valorBinary.length(); i > 0; i--) {
-            valor += Integer.parseInt(valorBinary.charAt(i - 1) + "") * Math.pow(2, (valorBinary.length() - i));
+        for (int i = valueBinary.length(); i > 0; i--) {
+            offset += Integer.parseInt(valueBinary.charAt(i - 1) + "") * Math.pow(2, (valueBinary.length() - i));
         }
 
-        return valor;
+        return offset;
     }
 
     /**
      * BINARY TO OCTAL
      */
 
-    public String converterBinaryParaOctal(String binary) {
+    public String converterBinaryToOctal(String binary) {
         StringBuilder sb = new StringBuilder();
         int initPos;
         int finalPos;
@@ -101,7 +101,7 @@ public class Converter extends Activity {
         while (initPos > 0) {
             initPos = ((initPos - 3) >= 0) ? initPos - 3 : 0;
 
-            sb.insert(0, converterBinaryParaDecimal(binary.substring(initPos, finalPos)));
+            sb.insert(0, converterBinaryToDecimal(binary.substring(initPos, finalPos)));
             finalPos = initPos;
         }
 
@@ -112,7 +112,7 @@ public class Converter extends Activity {
      * BINARY TO HEXADECIMAL
      */
 
-    public String converterBinaryParaHexadecimal(String binary) {
+    public String converterBinaryToHexadecimal(String binary) {
         char[] hex = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
         StringBuilder sb = new StringBuilder();
         int initPos;
@@ -125,7 +125,7 @@ public class Converter extends Activity {
         while (initPos > 0) {
             initPos = ((initPos - 4) >= 0) ? initPos - 4 : 0;
 
-            foundChar = hex[(int) converterBinaryParaDecimal(binary.substring(initPos, finalPos))];
+            foundChar = hex[(int) converterBinaryToDecimal(binary.substring(initPos, finalPos))];
             sb.insert(0, foundChar);
 
             finalPos = initPos;
@@ -138,8 +138,8 @@ public class Converter extends Activity {
      * OCTAL TO BINARY
      */
 
-    public String converterOctalParaBinary(String valorOctal) {
-        long decimal = Long.parseLong(valorOctal, 8);
+    public String converterOctalToBinary(String valueOctal) {
+        long decimal = Long.parseLong(valueOctal, 8);
         return Long.toBinaryString(decimal);
     }
 
@@ -147,31 +147,31 @@ public class Converter extends Activity {
      * OCTAL TO DECIMAL
      */
 
-    public long converterOctalParaDecimal(String valorOctal) {
-        long valor = 0;
+    public long converterOctalToDecimal(String valueOctal) {
+        long value = 0;
 
-        for (int i = valorOctal.length(); i > 0; i--) {
-            valor += Integer.parseInt(valorOctal.charAt(i - 1) + "") * Math.pow(8, (valorOctal.length() - i));
+        for (int i = valueOctal.length(); i > 0; i--) {
+            value += Integer.parseInt(valueOctal.charAt(i - 1) + "") * Math.pow(8, (valueOctal.length() - i));
         }
 
-        return valor;
+        return value;
     }
 
     /**
      * OCTAL TO HEXADECIMAL
      */
 
-    public String converterOctalParaHexadecimal(String valueOctal) {
-        long valorDecimal = converterOctalParaDecimal(valueOctal);
+    public String converterOctalToHexadecimal(String valueOctal) {
+        long valueDecimal = converterOctalToDecimal(valueOctal);
 
-        return converterDecimalParaHexadecimal(valorDecimal);
+        return converterDecimalToHexadecimal(valueDecimal);
     }
 
     /**
      * HEXADECIMAL TO BINARY
      */
 
-    public String converterHexadecimalParaBinary(String valueHex) {
+    public String converterHexadecimalToBinary(String valueHex) {
         long decimal = Long.parseLong(valueHex, 16);
         return Long.toBinaryString(decimal);
     }
@@ -180,7 +180,7 @@ public class Converter extends Activity {
      * HEXADECIMAL TO DECIMAL
      */
 
-    public long converterHexadecimalParaDecimal(String valueHex) {
+    public long converterHexadecimalToDecimal(String valueHex) {
         return Long.parseLong(valueHex, 16);
     }
 
@@ -188,7 +188,7 @@ public class Converter extends Activity {
      * HEXADECIMAL TO OCTAL
      */
 
-    public String converterHexadecimalParaOctal(String valueHex) {
+    public String converterHexadecimalToOctal(String valueHex) {
         long decimal = Long.parseLong(valueHex, 16);
         return Long.toOctalString(decimal);
     }
